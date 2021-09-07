c597622b4e770d38f323c2d054d1cab71b9d1f8e
Magdalena Brejna <magdalena.brejna@wp.pl>
2021-09-07 21:02:28 +0200

    feat: Create application appearance
    Create the css file with appearance rules. Apply rules in the
    application through the SceneBuilder. Add the method setStageStyle
    in the main class that set application background image.

--- src/main/java/pl/magdalena/brejna/colourtheworldapp/Main.java
+++ src/main/java/pl/magdalena/brejna/colourtheworldapp/Main.java
@@ -2,7 +2,8 @@ package pl.magdalena.brejna.colourtheworldapp;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import pl.magdalena.brejna.colourtheworldapp.utils.FxmlUtils;
 
@@ -29,15 +30,26 @@ public class Main extends Application {
         launch(args);
     }
 
+    private void setStageStyle(Stage stage){
+        Image image = new Image ("https://img.freepik.com/free-photo/recycled-crumpled-white-paper-texture-paper-background_34070-1016.jpg?size=626&ext=jpg");
+        mainPane.setBackground(new Background(new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true))));
+        stage.setFullScreen(true);
+    }
+
     public void start(Stage stage)  {
         setPrimaryStage(stage);
         mainPane = FxmlUtils.fxmlLoader(MAIN_LAYOUT_FXML);
         Scene scene = new Scene(mainPane);
 
+        setStageStyle(stage);
+
         primaryStage.setMinHeight(500);
         primaryStage.setMinWidth(850);
         primaryStage.setScene(scene);
-        primaryStage.setTitle(FxmlUtils.getResourceBundle().getString("title.application"));
         primaryStage.show();
     }
 }
--- src/main/java/pl/magdalena/brejna/colourtheworldapp/controllers/MainMenuButtonsController.java
+++ src/main/java/pl/magdalena/brejna/colourtheworldapp/controllers/MainMenuButtonsController.java
@@ -1,14 +1,23 @@
 package pl.magdalena.brejna.colourtheworldapp.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import pl.magdalena.brejna.colourtheworldapp.App;
 
 public class MainMenuButtonsController {
 
    private final String MAIN_PROJECT_FXML = "/fxml.files/MainProjectLayout.fxml";
 
+   @FXML
+   Button addProject;
+
    private MainLayoutController mainLayoutController;
 
+  /* public void init(){
+       addProject = new Button();
+       addProject.setId("add-project");
+   }
+*/
    public void setMainLayoutController(MainLayoutController mainController) {
        this.mainLayoutController = mainController;
    }
@@ -16,5 +25,6 @@ public class MainMenuButtonsController {
    @FXML
    private void openProject(){
        App.setCenterLayout(MAIN_PROJECT_FXML);
+
    }
 }
